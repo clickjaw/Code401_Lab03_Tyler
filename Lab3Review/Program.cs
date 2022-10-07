e542046680b753cc928c92cd1d83d5bbf4db1ffd
Tyler Morgan <clickjaw@outlook.com>
2022-10-07 00:23:03 -0500

    last one for the night

--- Lab3Review/Program.cs
+++ Lab3Review/Program.cs
@@ -16,6 +16,7 @@ namespace Lab3Review
             FourNumbers();
             BuildDiamond();
             MaxNumber();
+            BuildDiamond();
 
             FileCreateNewFile(newFilePath);
 
